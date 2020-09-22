c1c3c4edb9c1f6c142c407a6f9c17da684147814
avacallery <avacallery@gmail.com>
2020-09-22 13:37:50 -0400

    remove comments

--- src/com/avacallery/Console.java
+++ src/com/avacallery/Console.java
@@ -8,13 +8,6 @@ public class Console {
     // static class, NOT dynamic -> all methods in this class are static
     // variables do not need to be instantiated here
 
-    // user types "START", returns array of dice
-    // prompt: "Roll 2: Which die would you like to re-roll?"
-    // user types either 1, 2, 3, 4, 5 or ALL (?)
-    // prompt: "Roll 3: Which die would you like to re-roll?"
-    // user types either 1, 2, 3, 4, 5 or ALL (?)
-    // returns final roll of dice
-
     static Scanner scanner = new Scanner(System.in);
 
     static public void startGame() {
